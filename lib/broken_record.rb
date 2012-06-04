3ca9fcbe1ecf70441c486819251ea1617022b0e3
Gregory Brown <gregory.t.brown@gmail.com>
2012-06-04 12:53:43 -0400

    Tiny rearrangement for cleanup purposes

--- lib/broken_record.rb
+++ lib/broken_record.rb
@@ -1,8 +1,9 @@
-require_relative "broken_record/table"
-require_relative "broken_record/mapping"
-require_relative "broken_record/table_mapper"
-require_relative "broken_record/row_mapper"
 require_relative "broken_record/composable"
+require_relative "broken_record/row"
+require_relative "broken_record/table"
+require_relative "broken_record/row_mapper"
+require_relative "broken_record/table_mapper"
+require_relative "broken_record/mapping"
 
 module BrokenRecord
   class << self
