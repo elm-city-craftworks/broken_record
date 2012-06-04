2999998cc0d5ae6ddce759625b7a272260542fa9
Gregory Brown <gregory.t.brown@gmail.com>
2012-06-04 18:00:01 -0400

    Further refactorings

--- lib/broken_record.rb
+++ lib/broken_record.rb
@@ -8,5 +8,9 @@ require_relative "broken_record/mapping"
 module BrokenRecord
   class << self
     attr_accessor :database
+
+    def string_to_constant(constant_name)
+      constant_name.split("::").inject(Object) { |s,e| s.const_get(e) }
+    end
   end
 end
--- lib/broken_record/table_mapper/associations.rb
+++ lib/broken_record/table_mapper/associations.rb
@@ -6,32 +6,36 @@ module BrokenRecord
       end
 
       def belongs_to(parent, params)
-        mapper.record_class.send(:define_method, parent) do
-          Object.const_get(params[:class]).find(send(params[:key]))
+        define_association(parent) do
+          BrokenRecord.string_to_constant(params[:class])
+                      .find(send(params[:key]))
         end
       end
 
       def has_many(children, params)
         table_primary_key = mapper.primary_key
 
-        mapper.record_class.send(:define_method, children) do
-          Object.const_get(params[:class])
-                .where(params[:key] => send(table_primary_key))
+        define_association(children) do
+          BrokenRecord.string_to_constant(params[:class])
+            .where(params[:key] => send(table_primary_key))
         end
       end
 
       def has_one(child, params)
         table_primary_key = mapper.primary_key
 
-        mapper.record_class.send(:define_method, child) do
-          Object.const_get(params[:class])
-                .where(params[:key] => send(table_primary_key))
-                .first
+        define_association(child) do
+          BrokenRecord.string_to_constant(params[:class]).
+            where(params[:key] => send(table_primary_key)).first
         end
       end
 
       private
 
+      def define_association(name, &block)
+        mapper.record_class.send(:define_method, name, &block)
+      end
+      
       attr_accessor :mapper
     end
   end
